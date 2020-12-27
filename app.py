364f60bd8a0554fc663351978017b4661e4c0a27
Salih Alcı <salihalci@gmail.com>
2020-12-27 18:36:47 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app= Flask(__name__)
+
+@app.route('/')
+def index():
+    return ("Hello World")
+
+@app.route('/warmup')
+def warmup():
+    return ("warmup")
+
+@app.route('/test')
+def test():
+    return ("test")
+
+if __name__=="__main__":
+    app.run(debug=True)
