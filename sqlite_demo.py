e293ac4fdb566de8178ece5e2425db20a4738b4a
Salih Alci <salihalci@gmail.com>
2020-12-30 18:19:35 +0300

    added fetch line for sqlite

--- sqlite_demo.py
+++ sqlite_demo.py
@@ -2,8 +2,8 @@ import sqlite3
 
 conn = sqlite3.connect("test.db")
 
-
-
+c=conn.cursor()
+#Creates table on db
 #c=conn.execute("""CREATE TABLE TESTTABLE(
 #            first text,
 #            last text,
@@ -11,6 +11,13 @@ conn = sqlite3.connect("test.db")
 #            ) """)
 #"""
 
-c.execute("INSERT INTO test values('FirstName','LastName',1000)")
-conn.commit
+# Insert test data
+c.execute("INSERT INTO testtable values(datetime('now'),datetime('now', 'localtime'),3000)")
+conn.commit()
+
+c.execute("SELECT * FROM TESTTABLE")
+
+print(c.fetchall())
+
+
 conn.close()
